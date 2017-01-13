4f7b23de3379d6d879dce6f06db86a748f93fdc1
Alexey Abramov <alexey.abramov.salzi@gmail.com>
2017-01-13 08:32:00 +0100

    M: layer for data augmentation and its usage.

--- python/python-layers/DataAugmentation.py
+++ python/python-layers/DataAugmentation.py
@@ -8,6 +8,8 @@
 # augmented and non-augmented versions of the sample. That is also the way the built-in augmentation (mirroring) works.
 #
 
+# images in the network appear in the following format: <batch_size N><channels K><height H><width W>
+
 
 import caffe
 import numpy as np
@@ -26,7 +28,10 @@ class DataAugmentationLayer(caffe.Layer):
         params = eval(self.param_str)
         self.augmentation = np.array(params['augmentation'])
         self.mean_val = np.array(params['mean'])
-        self.mean_img = cv2.imread('mean-image-scaled.png',cv2.CV_LOAD_IMAGE_COLOR)
+        #self.mean_img = cv2.imread('mean-image-scaled.png',cv2.CV_LOAD_IMAGE_COLOR)
+        
+        #print 'self.augmentation = ', self.augmentation
+        #print 'self.mean_val = ', self.mean_val
 
 
     def reshape(self, bottom, top):
@@ -44,30 +49,36 @@ class DataAugmentationLayer(caffe.Layer):
         #print
         #print '--- DataAugmentationLayer: forward'
         #print
-
+        
+        #print 'bottom shape = ', bottom[0].data[...].shape        
+        #cv2.imwrite('bottom-0.png', bottom[0].data[0][...].transpose((1,2,0)))
+        
         if self.augmentation:
-
-            if random.random() > 0.5:
-                #print
-                #print '=====> apply augmentation ---'
-                #print
-
+            
+            #print '---> bottom data shape = ', bottom[0].data.shape[0]
+            
+            if random.random() > 0.2:
                 # do data augmentation for the whole batch
                 for ind in range(bottom[0].data.shape[0]):
+                    
+                    # randomly jitter contrast
                     bottom[0].data[ind][...] *= random.uniform(0.2,3.0)
                     bottom[0].data[ind][...][(bottom[0].data[ind][...] > 255.).nonzero()] = 255.
-
-                    #a = np.zeros([227, 227, 3], dtype=float)
-                    #a[:, :, 0] = bottom[0].data[ind][0, :, :]
-                    #a[:, :, 1] = bottom[0].data[ind][1, :, :]
-                    #a[:, :, 2] = bottom[0].data[ind][2, :, :]
-                    #cv2.imwrite('/home/alexey/augmentation/aug-' + str(ind) + '.png', a)
+                    
+                    #cv2.imwrite('augmentation/' + str(ind) + '-0.png', bottom[0].data[ind][...].transpose((1,2,0)))
+                    
+                    # horizontal flip
+                    #bottom[0].data[ind][0,:,:] = np.fliplr(bottom[0].data[ind][0,:,:])
+                    #bottom[0].data[ind][1,:,:] = np.fliplr(bottom[0].data[ind][1,:,:])
+                    #bottom[0].data[ind][2,:,:] = np.fliplr(bottom[0].data[ind][2,:,:])
+                    
+                    #cv2.imwrite('augmentation/' + str(ind) + '-1.png', bottom[0].data[ind][...].transpose((1,2,0)))
 
         # subtract the image mean for each image in the batch
-        for ind in range(bottom[0].data.shape[0]):
-            bottom[0].data[ind][0, :, :] -= self.mean_img[:, :, 0]
-            bottom[0].data[ind][1, :, :] -= self.mean_img[:, :, 1]
-            bottom[0].data[ind][2, :, :] -= self.mean_img[:, :, 2]
+        #for ind in range(bottom[0].data.shape[0]):
+        #    bottom[0].data[ind][0, :, :] -= self.mean_img[:, :, 0]
+        #    bottom[0].data[ind][1, :, :] -= self.mean_img[:, :, 1]
+        #    bottom[0].data[ind][2, :, :] -= self.mean_img[:, :, 2]
 
         top[0].reshape(*bottom[0].shape)
         top[0].data[...] = bottom[0].data[...]
